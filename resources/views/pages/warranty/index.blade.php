b80a83845c6188f9f7321ff76702a1667d29eb62
apirak <apirak@hafele.co.th>
2023-07-11 14:00:40 +0700

    update

---
+++ resources/views/pages/warranty/index.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.appform')
+
+@section('content')
+<!-- End Navbar -->
+<main class="main-content  mt-0">
+    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+      <div style="width: 100%;z-index: 9;text-align: center;">
+        <img src="https://www.hafelethailand.com/wp-content/uploads/2022/10/cropped-Hafele-Logo-white.png" style="z-index: 9;width: 250px;">
+      </div>
+      <span class="mask bg-primary opacity-6" style=""></span>
+    </div>
+    <div class="container">
+        <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
+            <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+                <div class="card z-index-0">
+                    <div class="card-header text-center pt-4">
+                        <h5>Warranty registration</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <form role="form">
+                            <div class="mb-3">
+                                <input type="text" class="form-control" placeholder="Name" aria-label="Name">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" placeholder="Email" aria-label="Email">
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" class="form-control" placeholder="Password"
+                                    aria-label="Password">
+                            </div>
+                            <div class="form-check form-check-info text-start">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    I agree the <a href="javascript:;" class="text-dark font-weight-bolder">Terms and
+                                        Conditions</a>
+                                </label>
+                            </div>
+                            <div class="text-center">
+                                <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                            </div>
+                            <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;"
+                                    class="text-dark font-weight-bolder">Sign in</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+<footer class="footer py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 mb-4 mx-auto text-center">
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Company
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    About Us
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Team
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Products
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Blog
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                    Pricing
+                </a>
+            </div>
+            <div class="col-lg-8 mx-auto text-center mb-4 mt-2">
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-dribbble"></span>
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-twitter"></span>
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-instagram"></span>
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-pinterest"></span>
+                </a>
+                <a href="javascript:;" target="_blank" class="text-secondary me-xl-4 me-4">
+                    <span class="text-lg fab fa-github"></span>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 mx-auto text-center mt-1">
+                <p class="mb-0 text-secondary">
+                    Copyright ©
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script> Soft by Creative Tim.
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+@endsection
