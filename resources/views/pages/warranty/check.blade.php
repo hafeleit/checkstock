641a2bb155436155bf7fa9d136ecfd8ace180bf1
apirak <apirak@hafele.co.th>
2023-08-18 14:49:10 +0700

    update

--- resources/views/pages/warranty/check.blade.php
+++ resources/views/pages/warranty/check.blade.php
@@ -46,7 +46,7 @@
                         <form role="form">
                             <div class="mb-3">
                                 <label class="img text-sm">Search serial no.<span class="text-danger"></span></label>
-                                <input id="search" name="search" type="text" class="form-control" placeholder="serial no 16 - 20 digit" value="989898998989989889">
+                                <input id="search" name="search" type="text" class="form-control" placeholder="serial no 16 - 20 digit">
                             </div>
                             <div class="card-body p-0 pb-0" id="get_warranty">
 
