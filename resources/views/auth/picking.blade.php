e95a14ccdbc030fdd4695706a0f0c4df374d62a8
apirak <apirak@hafele.co.th>
2023-09-18 14:59:54 +0700

    update

--- resources/views/auth/picking.blade.php
+++ resources/views/auth/picking.blade.php
@@ -8,9 +8,10 @@
 </style>
     <main class="main-content  mt-0">
         <section>
+
                 <div class="container">
                     <div class="row">
-                        <div id="tab-login" class="col-xl-3 col-lg-5 col-md-7 mx-lg-0" style="">
+                        <div id="tab-login" class="col-xl-4 col-lg-5 col-md-7 mx-lg-0" style="">
                             <div class="card card-plain">
                                 <div class="card-header pb-0 text-start">
                                     <h5 class="font-weight-bolder">LOGIN</h5>
@@ -36,59 +37,77 @@
                                   </table>
 
                                   <div class="text-center">
-                                      <button id="btn-next" type="button" class="btn btn-sm btn-primary btn-sm w-60 mt-4 mb-0">Next</button>
+                                      <button id="btn-next" type="button" class="btn btn-sm btn-primary btn-sm w-40 mt-4 mb-0">Next</button>
                                   </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div id="tab-picking" class="col-xl-3 col-lg-5 col-md-7 mx-lg-0" style=" display:none">
-                            <div class="card card-plain">
-				<div class="card-header pb-0 text-start">
-                                    <h5 class="font-weight-bolder">PICKING</h5>
-                                </div>
-                                <div class="card-body">
-                                  <table>
-                                    <tr>
-                                      <td class="input-sm" align="right">Ticket:</td>
-                                      <td align="right"><input type="text" name="" class="input-sm" size="15"><i class="ni ni-app"></i></td>
-                                    </tr>
-                                    <tr>
-                                      <td class="input-sm" align="right">Position: </td>
-                                      <td><input type="text" name="" class="input-sm" size="15"></td>
-                                    </tr>
-                                    <tr>
-                                      <td class="input-sm" align="right">Item/G1/G2: </td>
-                                      <td><input type="text" name="" class="input-sm" size="15"></td>
-                                    </tr>
-                                    <tr>
-                                      <td class="input-sm" align="right">Item Desc: </td>
-                                      <td><input type="text" name="" class="input-sm" size="15"></td>
-                                    </tr>
-                                    <tr>
-                                      <td class="input-sm" align="right">Batch/Serial:</td>
-                                      <td><input type="text" name="" class="input-sm" size="15"><i class="ni ni-air-baloon"></i></td>
-                                    </tr>
-                                    <tr>
-                                      <td class="input-sm" align="right">Pack Code: </td>
-                                      <td><input type="text" name="" class="input-sm" size="15"></td>
-                                    </tr>
-                                    <tr>
-                                      <td class="input-sm" align="right">Pack Qty.: </td>
-                                      <td><input type="text" name="" class="input-sm" size="6"><input type="text" name="" class="input-sm" size="6"></td>
-                                    </tr>
-                                    <tr>
-                                      <td class="input-sm" align="right">Base Qty.: </td>
-                                      <td><input type="text" name="" class="input-sm" size="15" readonly></td>
-                                    </tr>
-                                  </table>
+                    <div id="tab-picking" class="row" style="display: none;">
+                        <div class="col-xl-4 col-lg-5 col-md-7 mx-lg-0">
 
-                                  <div class="text-center">
-                                      <button id="btn-save" type="button" class="btn btn-sm btn-primary btn-sm w-60 mt-4 mb-0">Save</button>
-                                  </div>
+                          <div class="nav-wrapper position-relative end-0">
+                              <ul class="nav nav-pills nav-fill p-1" role="tablist">
+                                  <li class="nav-item">
+                                      <a class="nav-link mb-0 px-0 py-1 active d-flex align-items-center justify-content-center "data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="true">
+                                          <span class="ms-2">PICKING</span>
+                                      </a>
+                                  </li>
+                                  <li class="nav-item">
+                                      <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center "data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
+                                        <span class="ms-2">MENU</span>
+                                      </a>
+                                  </li>
+                                  <li class="nav-item">
+                                      <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center "data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
+                                          <span class="ms-2">MENU</span>
+                                      </a>
+                                  </li>
+                              </ul>
+                          </div>
+
+                          <div class="card card-plain">
+                              <div class="card-body">
+                                <table>
+                                  <tr>
+                                    <td class="input-sm" align="right">Ticket:</td>
+                                    <td align="right"><input type="text" name="" class="input-sm" size="15"><i class="ni ni-app"></i></td>
+                                  </tr>
+                                  <tr>
+                                    <td class="input-sm" align="right">Position: </td>
+                                    <td><input type="text" name="" class="input-sm" size="15"></td>
+                                  </tr>
+                                  <tr>
+                                    <td class="input-sm" align="right">Item/G1/G2: </td>
+                                    <td><input type="text" name="" class="input-sm" size="15"></td>
+                                  </tr>
+                                  <tr>
+                                    <td class="input-sm" align="right">Item Desc: </td>
+                                    <td><input type="text" name="" class="input-sm" size="15"></td>
+                                  </tr>
+                                  <tr>
+                                    <td class="input-sm" align="right">Batch/Serial:</td>
+                                    <td><input type="text" name="" class="input-sm" size="15"><i class="ni ni-air-baloon"></i></td>
+                                  </tr>
+                                  <tr>
+                                    <td class="input-sm" align="right">Pack Code: </td>
+                                    <td><input type="text" name="" class="input-sm" size="15"></td>
+                                  </tr>
+                                  <tr>
+                                    <td class="input-sm" align="right">Pack Qty.: </td>
+                                    <td><input type="text" name="" class="input-sm" size="6"><input type="text" name="" class="input-sm" size="6"></td>
+                                  </tr>
+                                  <tr>
+                                    <td class="input-sm" align="right">Base Qty.: </td>
+                                    <td><input type="text" name="" class="input-sm" size="15" readonly></td>
+                                  </tr>
+                                </table>
+
+                                <div class="text-center">
+                                    <button id="btn-save" type="button" class="btn btn-sm btn-primary btn-sm w-40 mt-4 mb-0">Save</button>
                                 </div>
-                            </div>
+                              </div>
+                          </div>
                         </div>
                     </div>
                 </div>
@@ -101,6 +120,7 @@
       $('#btn-next').on('click', function(){
         $('#tab-login').css('display','none');
         $('#tab-picking').css('display','');
+        $('.moving-tab').css('width','143px');
       });
 
       $('#btn-save').on('click', function(){
