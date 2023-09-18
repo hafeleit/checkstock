43c2d331fe0f181f644a92281172fe83e603b62b
apirak <apirak@hafele.co.th>
2023-09-18 17:02:39 +0700

    update

--- resources/views/auth/picking.blade.php
+++ resources/views/auth/picking.blade.php
@@ -11,7 +11,7 @@
 
                 <div class="container">
                     <div class="row">
-                        <div id="tab-login" class="col-xl-4 col-lg-5 col-md-7 mx-lg-0" style="">
+                        <div id="tab-login" class="col-xl-5 col-lg-5 col-md-7 mx-lg-0" style="">
                             <div class="card card-plain">
                                 <div class="card-header pb-0 text-start">
                                     <h5 class="font-weight-bolder">LOGIN</h5>
@@ -45,33 +45,32 @@
                     </div>
 
                     <div id="tab-picking" class="row" style="display: none;">
+
                       <div class="col-lg-4 col-md-6 my-sm-auto ms-sm-auto me-sm-0 mx-auto mt-3">
                           <div class="nav-wrapper position-relative end-0">
                               <ul class="nav nav-pills nav-fill p-1" role="tablist">
                                   <li class="nav-item">
                                       <a class="nav-link mb-0 px-0 py-1 active d-flex align-items-center justify-content-center "
                                           data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="true">
-                                          <i class="ni ni-app"></i>
-                                          <span class="ms-2">App</span>
+                                          <span class="ms-2">PICKING</span>
                                       </a>
                                   </li>
                                   <li class="nav-item">
                                       <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center "
                                           data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
-                                          <i class="ni ni-email-83"></i>
-                                          <span class="ms-2">Messages</span>
+                                          <span class="ms-2">PRODUCT</span>
                                       </a>
                                   </li>
                                   <li class="nav-item">
                                       <a class="nav-link mb-0 px-0 py-1 d-flex align-items-center justify-content-center "
                                           data-bs-toggle="tab" href="javascript:;" role="tab" aria-selected="false">
-                                          <i class="ni ni-settings-gear-65"></i>
                                           <span class="ms-2">Settings</span>
                                       </a>
                                   </li>
                               </ul>
                           </div>
                       </div>
+
                         <div class="col-xl-4 col-lg-5 col-md-7 mx-lg-0">
                           <div class="card card-plain">
                               <div class="card-body">
@@ -130,7 +129,7 @@
         $('#tab-login').css('display','none');
         $('#tab-picking').css('display','');
         $('.moving-tab').css('width','35%');
-        $('#ticket').focus();
+        //$('#ticket').focus();
       });
 
       $('#btn-save').on('click', function(){
