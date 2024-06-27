0aaa90ada6ebb3fdfa6b62f4882b0b670fbbc914
apirak <hthithafele@gmail.com>
2024-06-27 14:08:24 +0700

    update

--- resources/views/auth/itservice_thank.blade.php
+++ resources/views/auth/itservice_thank.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.appform')
 
 @section('content')
     @include('layouts.navbars.guest.navbar')
-  
+
     <main class="main-content  mt-0">
         <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
             style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
