b4eb65d08b8bf848ffb2ec7e2d9b1736b2bb370d
apirak <hthithafele@gmail.com>
2024-11-02 15:54:53 +0700

    p

--- resources/views/pages/clr_dashboard.blade.php
+++ resources/views/pages/clr_dashboard.blade.php
@@ -63,7 +63,7 @@
                                     <p class="text-sm mb-0 text-uppercase font-weight-bold">Day 3</p>
                                     <h2 class="font-weight-bolder">
                                         {{ ($clr_total['day3_total'] > 0) ? number_format($clr_total['day3_total']) : '-' }}
-                                        <p class="mb-0" style="position: absolute;bottom: 4px;right: 20px;font-size: 14px;">Last update: 14:00</p>
+                                        <p class="mb-0" style="position: absolute;bottom: 4px;right: 20px;font-size: 14px;">Last update: 16:00</p>
                                     </h2>
 
                                 </div>
