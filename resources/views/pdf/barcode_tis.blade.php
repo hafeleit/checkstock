a2029e1be44945486e7ae786ad3244121a7c5704
apirak <apirak@hafele.co.th>
2024-11-08 10:21:35 +0700

    update

--- resources/views/pdf/barcode_tis.blade.php
+++ resources/views/pdf/barcode_tis.blade.php
@@ -8,7 +8,7 @@
     body {
     	margin-top: -42px;margin-left: -42px;
     	font-family: 'AngsanaUPC'; height: 100%;
-      font-size: 16px;
+      font-size: 13px;
     }
     .container{
       position:absolute;
@@ -19,20 +19,18 @@
       height: 400px;
       width: 580px;
       z-index: -1;
-      padding: 3px 0px 0px 0px;
+      padding: 1.5px 0px 0px 0px;
     }
-
     tr {
-      line-height: 6.5px;
+      line-height: 5.5px;
     }
-
   </style>
 
-  <div class="" style="position:absolute;right:-30px; z-index: -1">
+  <!--<div class="" style="position:absolute;right:-30px; z-index: -1">
     <img src="img/logos/Logo-HAFELE-02.jpg" width="120" style="margin-top:2px">
-  </div>
-  <div class="" style="position:absolute; right:-30px;top:85px; text-align:center">
-    <img src="img/logos/LEAD.png" width="80" style="margin-bottom: -28px;"></br>
+  </div>-->
+  <div class="" style="position:absolute; right:-20px;top:50px; text-align:center">
+    <img src="img/logos/LEAD.png" width="80" style="margin-bottom: -24px;"></br>
     <span style="text-align:center;font-weight:Bold">{{ $productItems["tis_1"] }}</span>
   </div>
   @if($productItems["qr_code"] != '')
