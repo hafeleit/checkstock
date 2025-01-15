285e131d7253af7dcde1ef7a57759e323dd89248
apirak <apirak@hafele.co.th>
2025-01-15 18:07:30 +0700

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -618,6 +618,34 @@ class OrderController extends Controller
 
     }
 
+    public function slack_api($message){
+
+      $curl = curl_init();
+
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://slack.com/api/chat.postMessage',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS =>'{
+        "channel": "C088H8QFRHC",
+        "text": "'.$message.'"
+      }',
+        CURLOPT_HTTPHEADER => array(
+          'Authorization: Bearer ' . config('services.slack.api_token'),
+          'Content-Type: application/json'
+        ),
+      ));
+
+      $response = curl_exec($curl);
+
+      curl_close($curl);
+    }
+
     public function onlineorder_manual_get(){
 
       $new_order = [];
--- config/services.php
+++ config/services.php
@@ -31,4 +31,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'slack' => [
+        'api_token' => env('SLACK_API_TOKEN'),
+    ],
+
 ];
