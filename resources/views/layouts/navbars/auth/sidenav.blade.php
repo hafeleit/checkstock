37edb5b967a10b0fd5ec0573d3e163fe4733db59
apirak <apirak@hafele.co.th>
2025-03-13 11:37:08 +0700

    update

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -156,7 +156,7 @@
           <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="ni ni-archive-2 {{ Request::segment(1) == 'sales-usi' ? 'text-primary' : 'text-dark' }} text-sm opacity-10"></i>
           </div>
-          <span class="nav-link-text ms-1">SALES USI</span>
+          <span class="nav-link-text ms-1">Sales USI</span>
         </a>
       </li>
       @endcan
