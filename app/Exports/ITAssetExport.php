cf218d7c28c8394edbd1716dcec66938e1e3271f
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2025-07-30 13:08:22 +0700

    :sparkles: feat: add software column to excel

--- app/Exports/ITAssetExport.php
+++ app/Exports/ITAssetExport.php
@@ -9,17 +9,45 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 class ITAssetExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        $res = ITAsset::leftJoin('i_t_asset_owns','i_t_assets.computer_name','i_t_asset_owns.computer_name')
-                          ->leftJoin('user_masters','i_t_asset_owns.user','user_masters.job_code')
-                          ->leftJoin('i_t_asset_types','i_t_asset_types.type_code','i_t_assets.type')
-                          ->select('i_t_assets.*','i_t_asset_owns.user','user_masters.name_en','user_masters.dept','user_masters.position','i_t_asset_types.type_desc')
-                          ->groupBy('i_t_assets.computer_name')
-                          ->where('i_t_assets.delete','0')
-                          ->get();
+        $res = ITAsset::leftJoin('i_t_asset_owns', 'i_t_assets.computer_name', 'i_t_asset_owns.computer_name')
+            ->leftJoin('user_masters', 'i_t_asset_owns.user', 'user_masters.job_code')
+            ->leftJoin('i_t_asset_types', 'i_t_asset_types.type_code', 'i_t_assets.type')
+            ->leftjoin('softwares', 'softwares.computer_name', 'i_t_assets.computer_name')
+            ->select(
+                'i_t_assets.id',
+                'i_t_assets.computer_name',
+                'i_t_assets.serial_number',
+                'i_t_assets.type as type_code',
+                'i_t_assets.color',
+                'i_t_assets.model',
+                'i_t_assets.fixed_asset_no',
+                'i_t_assets.purchase_date',
+                'i_t_assets.warranty',
+                'i_t_assets.status',
+                'i_t_assets.location',
+                'i_t_assets.create_by',
+                'i_t_assets.delete',
+                'i_t_assets.created_at',
+                'i_t_assets.updated_at',
+                \DB::raw('GROUP_CONCAT(softwares.software_name ORDER BY softwares.software_name ASC SEPARATOR \', \') AS software_name'),
+                'i_t_assets.old_user',
+                'i_t_assets.old_name',
+                'i_t_assets.old_department',
+                'i_t_assets.update_by',
+                'i_t_assets.reason_broken',
+                'i_t_asset_owns.user as job_code',
+                'user_masters.name_en as user_name',
+                'user_masters.dept',
+                'user_masters.position',
+                'i_t_asset_types.type_desc'
+            )
+            ->groupBy('i_t_assets.computer_name')
+            ->where('i_t_assets.delete', '0')
+            ->get();
 
         return $res;
     }
@@ -42,6 +70,7 @@ class ITAssetExport implements FromCollection, WithHeadings
             "delete",
             "created_at",
             "updated_at",
+            "software_name",
             "old_user",
             "old_name",
             "old_department",
@@ -52,6 +81,6 @@ class ITAssetExport implements FromCollection, WithHeadings
             "dept",
             "position",
             "type_desc",
-          ];
+        ];
     }
 }
