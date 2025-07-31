2012a1e8d431edf764f8674bf8207a1c3deae53c
apirak <apirak@hafele.co.th>
2025-07-31 09:00:11 +0700

    update

---
+++ database/migrations/2025_07_30_145036_create_commissions_schemas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commissions_schemas', function (Blueprint $table) {
+            $table->id();
+            $table->string('division_name');
+            $table->string('ar_start',10)->nullable();
+            $table->string('ar_end',10)->nullable();
+            $table->decimal('rate_percent', 8, 2)->default(0);
+            $table->string('version')->nullable();
+            $table->text('comment')->nullable();
+            $table->string('create_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commissions_schemas');
+    }
+};
