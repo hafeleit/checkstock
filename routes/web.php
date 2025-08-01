c5f9f091713b752790c3472d4f3529a3810cfbd8
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2025-08-01 09:07:19 +0700

    :lock: fix: add is_active check for direct route access
    Signed-off-by: rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,8 @@ use Spatie\Permission\Models\Role;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-
-
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 
@@ -47,65 +44,57 @@ use App\Http\Controllers\Consumerlabel\ProductItemsController;
 use App\Http\Controllers\ITAssetTypeController;
 use App\Http\Controllers\InvRecordController;
 
-  Route::get('/', function () {
-    //abort(404);
-    return view('welcome');
-  });
+Route::get('/', function () {
+  //abort(404);
+  return view('welcome');
+});
 
-  //warranty
-  Route::get('warranty', function(){ return Redirect::to(route('register-warranty.index')); });
-  Route::resource('register-warranty', WarrantyController::class);
-  Route::get('check-warranty', [WarrantyController::class, 'check_warranty'])->name('check_warranty');
-  Route::get('warranty-search-ajax', [WarrantyController::class, 'search_warranty'])->name('warranty.search_warranty');
+//warranty
+Route::get('warranty', function () {
+  return Redirect::to(route('register-warranty.index'));
+});
+Route::resource('register-warranty', WarrantyController::class);
+Route::get('check-warranty', [WarrantyController::class, 'check_warranty'])->name('check_warranty');
+Route::get('warranty-search-ajax', [WarrantyController::class, 'search_warranty'])->name('warranty.search_warranty');
 
-  //dashboard
-  Route::get('ass_dashboard', [HomeController::class, 'ass_dashboard'])->name('ass_dashboard');
-  Route::get('clr_dashboard', [HomeController::class, 'clr_dashboard'])->name('clr_dashboard');
+//dashboard
+Route::get('ass_dashboard', [HomeController::class, 'ass_dashboard'])->name('ass_dashboard');
+Route::get('clr_dashboard', [HomeController::class, 'clr_dashboard'])->name('clr_dashboard');
 
-  //check stock
-  //Route::resource('products', ProductController::class);
-  //Route::post('products-import', [ProductController::class, 'import'])->name('products.import');
-  //Route::get('products-search-ajax', [ProductController::class, 'search_ajax'])->name('products.search-ajax');
+//check stock
+//Route::resource('products', ProductController::class);
+//Route::post('products-import', [ProductController::class, 'import'])->name('products.import');
+//Route::get('products-search-ajax', [ProductController::class, 'search_ajax'])->name('products.search-ajax');
 
-  //so status
-
-
-  //sales usi
-
-  Route::get('search-usi', [SalesUSIController::class,'search_usi'])->name('search_usi');
-  Route::get('search-usi-inbound', [SalesUSIController::class,'inbound'])->name('search_inbound');
-  Route::get('search-usi-outbound', [SalesUSIController::class,'outbound'])->name('search_outbound');
-  //test
-  Route::get('send-mail', [MailController::class, 'index']);
-  Route::get('picking', [LoginController::class, 'picking']);
-  Route::get('test_db_crm', [HomeController::class, 'test_db']);
+//so status
 
 
+//sales usi
 
+Route::get('search-usi', [SalesUSIController::class, 'search_usi'])->name('search_usi');
+Route::get('search-usi-inbound', [SalesUSIController::class, 'inbound'])->name('search_inbound');
+Route::get('search-usi-outbound', [SalesUSIController::class, 'outbound'])->name('search_outbound');
+//test
+Route::get('send-mail', [MailController::class, 'index']);
+Route::get('picking', [LoginController::class, 'picking']);
+Route::get('test_db_crm', [HomeController::class, 'test_db']);
 
+Route::get('/itservice-rating/{task_id}', [RegisterController::class, 'itservice_rating'])->name('itservice-rating');
+Route::post('/itservice-rating/{task_id}', [RegisterController::class, 'save_itservice_rating'])->name('save-itservice-rating');
 
-
-
-
-
-
-
-  Route::get('/itservice-rating/{task_id}', [RegisterController::class, 'itservice_rating'])->name('itservice-rating');
-  Route::post('/itservice-rating/{task_id}', [RegisterController::class, 'save_itservice_rating'])->name('save-itservice-rating');
-
-	Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
-	Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
-	Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
-	Route::post('/login', [LoginController::class, 'login'])->middleware('guest')->name('login.perform');
-	Route::get('/reset-password', [ResetPassword::class, 'show'])->middleware('guest')->name('reset-password');
-	Route::post('/reset-password', [ResetPassword::class, 'send'])->middleware('guest')->name('reset.perform');
-	/*Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest')->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->middleware('guest')->name('register.perform');
+Route::get('/login', [LoginController::class, 'show'])->middleware('guest')->name('login');
+Route::post('/login', [LoginController::class, 'login'])->middleware('guest')->name('login.perform');
+Route::get('/reset-password', [ResetPassword::class, 'show'])->middleware('guest')->name('reset-password');
+Route::post('/reset-password', [ResetPassword::class, 'send'])->middleware('guest')->name('reset.perform');
+/*Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('guest')->name('change-password');
 	Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');*/
-	Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 
-Route::group(['middleware' => 'auth'], function () {
-//Route::group(['middleware' => ['role:super-admin|admin|staff|supplier|user']], function() {
+Route::middleware(['auth', 'check.status'])->group( function () {
+  //Route::group(['middleware' => ['role:super-admin|admin|staff|supplier|user']], function() {
   Route::get('/change-password', [ChangePassword::class, 'show'])->name('change-password');
   Route::post('/change-password', [ChangePassword::class, 'update'])->name('change.perform');
 
@@ -122,92 +111,90 @@ Route::group(['middleware' => 'auth'], function () {
   Route::put('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'givePermissionToRole']);
   Route::resource('users', App\Http\Controllers\UserController::class);
   Route::get('users/{userId}/delete', [App\Http\Controllers\UserController::class, 'destroy']);
-  Route::post('usermaster-import', [UserMasterController::class,'import'])->name('usermaster-import');
-  Route::get('itasset-export', [ITAssetController::class,'export'])->name('itasset-export');
+  Route::post('usermaster-import', [UserMasterController::class, 'import'])->name('usermaster-import');
+  Route::get('itasset-export', [ITAssetController::class, 'export'])->name('itasset-export');
   Route::resource('itasset', ITAssetController::class);
   Route::resource('asset_types', ITAssetTypeController::class);
   Route::resource('onlineorder', OrderController::class);
   Route::resource('checkstock', CheckStockController::class);
-  Route::get('checkstockhww-export', [CheckStockController::class,'export'])->name('checkstockhww-export');
-  Route::post('product-new-price-list-import', [CheckStockController::class,'import'])->name('product-new-price-list-import');
-  Route::get('onlineorder/download/{file}', [OrderController::class,'download'])->name('onlineorder-download');
-  Route::get('onlineorder-manual-get', [OrderController::class,'onlineorder_manual_get'])->name('onlineorder-manual-get');
+  Route::get('checkstockhww-export', [CheckStockController::class, 'export'])->name('checkstockhww-export');
+  Route::post('product-new-price-list-import', [CheckStockController::class, 'import'])->name('product-new-price-list-import');
+  Route::get('onlineorder/download/{file}', [OrderController::class, 'download'])->name('onlineorder-download');
+  Route::get('onlineorder-manual-get', [OrderController::class, 'onlineorder_manual_get'])->name('onlineorder-manual-get');
 
   //Consumerlabel
   Route::resource('product-items', ProductItemsController::class);
-  Route::get('/export-product-items', function(){
+  Route::get('/export-product-items', function () {
     return Excel::download(new ProductitemsExport, 'ProductItems.xlsx');
   })->name('productitems_export');
   Route::post('/import-product-items', function (Request $request) {
-      $request->validate([
-          'file' => 'required|mimes:xlsx,csv', // ตรวจสอบชนิดไฟล์
-      ]);
+    $request->validate([
+      'file' => 'required|mimes:xlsx,csv', // ตรวจสอบชนิดไฟล์
+    ]);
 
-      // Import ไฟล์
-      Excel::import(new ProductitemsImport, $request->file('file'));
+    // Import ไฟล์
+    Excel::import(new ProductitemsImport, $request->file('file'));
 
-      return back()->with('succes', 'Import ข้อมูลสำเร็จ!');
+    return back()->with('succes', 'Import ข้อมูลสำเร็จ!');
   })->name('productitems_import');
-  Route::get('consumerlabel-barcode', [ProductItemsController::class,'pdfbarcode'])->name('pdfbarcode');
+  Route::get('consumerlabel-barcode', [ProductItemsController::class, 'pdfbarcode'])->name('pdfbarcode');
   //routes template
-	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
-	Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
-	Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
-	Route::post('/profile', [UserProfileController::class, 'update'])->name('profile.update');
-	Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static');
-	Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
-	Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
-	//Route::get('/{page}', [PageController::class, 'index'])->name('page');
+  Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
+  Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
+  Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
+  Route::post('/profile', [UserProfileController::class, 'update'])->name('profile.update');
+  Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static');
+  Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
+  Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
+  //Route::get('/{page}', [PageController::class, 'index'])->name('page');
   Route::get('/user-management', [PageController::class, 'user_management'])->name('user-management');
   Route::get('/tables', [PageController::class, 'tables'])->name('tables');
   Route::get('/billing', [PageController::class, 'billing'])->name('billing');
-	Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+  Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
   Route::get('/import-users', function () {
 
-      $filePath = storage_path('app/users.xlsx');
+    $filePath = storage_path('app/users.xlsx');
 
-      if (!file_exists($filePath)) {
-          return '❌ ไม่พบไฟล์ users.xlsx';
+    if (!file_exists($filePath)) {
+      return '❌ ไม่พบไฟล์ users.xlsx';
+    }
+
+    $rows = Excel::toArray([], $filePath)[0];
+    $header = array_map('strtolower', $rows[0]);
+    unset($rows[0]);
+
+    $imported = 0;
+    $errors = [];
+
+    foreach ($rows as $index => $row) {
+      $data = array_combine($header, $row);
+
+      if (empty($data['role name'])) {
+        $errors[] = "❌ แถวที่ " . ($index + 2) . " ไม่มี role name";
+        continue;
       }
 
-      $rows = Excel::toArray([], $filePath)[0];
-      $header = array_map('strtolower', $rows[0]);
-      unset($rows[0]);
+      $roleName = trim($data['role name']);
+      $role = Role::where('name', $roleName)->first();
 
-      $imported = 0;
-      $errors = [];
-
-      foreach ($rows as $index => $row) {
-          $data = array_combine($header, $row);
-
-          if (empty($data['role name'])) {
-              $errors[] = "❌ แถวที่ ".($index + 2)." ไม่มี role name";
-              continue;
-          }
-
-          $roleName = trim($data['role name']);
-          $role = Role::where('name', $roleName)->first();
-
-          if (!$role) {
-              $errors[] = "❌ ไม่พบ Role: $roleName (แถวที่ ".($index + 2).")";
-              continue;
-          }
-
-          $user = User::updateOrCreate(
-              ['email' => $data['email']],
-              [
-                  'username' => $data['name'],
-                  'password' => $data['password'],
-                  'supp_code' => $data['supp_code'],
-              ]
-          );
-
-          $user->assignRole($role);
-          $imported++;
+      if (!$role) {
+        $errors[] = "❌ ไม่พบ Role: $roleName (แถวที่ " . ($index + 2) . ")";
+        continue;
       }
-      echo "✅ นำเข้าสำเร็จ $imported คน";
 
+      $user = User::updateOrCreate(
+        ['email' => $data['email']],
+        [
+          'username' => $data['name'],
+          'password' => $data['password'],
+          'supp_code' => $data['supp_code'],
+        ]
+      );
+
+      $user->assignRole($role);
+      $imported++;
+    }
+    echo "✅ นำเข้าสำเร็จ $imported คน";
   });
-
 });
