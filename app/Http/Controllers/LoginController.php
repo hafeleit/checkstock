cbd5826d07a0ce6475a07b78cc26f824a7b60940
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2025-08-11 12:00:05 +0700

    :sparkles: feat: add last login to internal

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
@@ -34,6 +35,7 @@ class LoginController extends Controller
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'is_active' => 1, 'type' => 'employee'])) {
             $request->session()->regenerate();
+            auth()->user()->update(['last_logged_in_at' => Carbon::now()]);
             return redirect()->intended('profile');
         }
 
