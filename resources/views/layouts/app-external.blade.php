4894f077e7e85fd95fa843311dd9c98e8b922a36
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2025-08-11 11:50:49 +0700

    :wrench: chore: fix mobile menu functionality

--- resources/views/layouts/app-external.blade.php
+++ resources/views/layouts/app-external.blade.php
@@ -85,7 +85,7 @@
                             <div class="px-4 py-2">
                                 <h3 class="text-xs font-ligth text-white uppercase tracking-wider">Main Menu</h3>
                             </div>
-                            <a href="#" class="block font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
+                            <a href="{{ route('customer.products.index') }}" class="block font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
                                 <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path>
                                 </svg>
@@ -97,25 +97,29 @@
                         <div class="px-4 py-2">
                             <h3 class="text-xs font-ligth text-white uppercase tracking-wider">Account</h3>
                         </div>
-                        <a href="#" class="block font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
+                        <a href="{{ route('customer.profile.show', auth()->user()->id) }}" class="block font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
                             <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg>
                             Profile
                         </a>
-                        <a href="#" class="block font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
+                        <a href="{{ route('customer.profile.change-password') }}" class="block font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
                             <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
                             </svg>
 
                             Change Password
                         </a>
-                        <a href="#" class="block font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
-                            <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
-                            </svg>
-                            Sign out
-                        </a>
+                        <form method="POST" action="{{ route('customer.logout') }}">
+                            @csrf
+                            <button type="submit"
+                                class="flex items-center w-full font-semibold px-4 py-2 hover:bg-red-800 flex items-center">
+                                <svg class="w-4 h-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                                </svg>
+                                <p>Sign out</p>
+                            </button>
+                        </form>
                     </nav>
                 </div>
             </div>
