675801e9bc1d8f5e0677e42a79c6da54dc2ec085
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2025-08-13 16:22:37 +0700

    :fire: refactor: change email login input type

--- resources/views/auth/external-login.blade.php
+++ resources/views/auth/external-login.blade.php
@@ -20,7 +20,7 @@
                         </svg>
                         Email address
                     </label>
-                    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus placeholder="Enter your email" class="w-full h-11 px-3 py-2 border border-gray-300 rounded-md shadow-sm @error('email') border-red-500 @enderror">
+                    <input id="email" type="text" name="email" value="{{ old('email') }}" required autofocus placeholder="Enter your email" class="w-full h-11 px-3 py-2 border border-gray-300 rounded-md shadow-sm @error('email') border-red-500 @enderror">
                     @error('email')
                     <p class="text-red-500 text-xs">{{ $message }}</p>
                     @enderror
