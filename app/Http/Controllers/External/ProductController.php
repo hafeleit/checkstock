c372b16d79e2098d5893d81e8f3654a515e5daaa
apirak <apirak@hafele.co.th>
2025-08-17 14:10:34 +0700

    update

--- app/Http/Controllers/External/ProductController.php
+++ app/Http/Controllers/External/ProductController.php
@@ -36,7 +36,7 @@ class ProductController extends Controller
                     ->select(
                         'ZHWWBCQUERYDIR.Material',
                         'ZHWWBCQUERYDIR.kurztext',
-                        DB::raw('ZORDPOSKONV_ZPL.Amount / NULLIF(ZORDPOSKONV_ZPL.per, 0) AS amount'),
+                        DB::raw('ZORDPOSKONV_ZPL.Amount / NULLIF(ZORDPOSKONV_ZPL.per, 0) AS Amount'),
                         'MB52.unrestricted'
                     )
                     ->first();
