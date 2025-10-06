68e9832b5340bd8889845c5713f076955b7061fe
apirak <apirak@hafele.co.th>
2025-10-06 10:25:25 +0700

    fix dashboard

--- resources/views/pages/clr_dashboard.blade.php
+++ resources/views/pages/clr_dashboard.blade.php
@@ -50,11 +50,6 @@
                                     <p class="text-sm mb-0 text-uppercase font-weight-bold">Day 2 (2025 | 2024)</p>
                                     <h2 class="font-weight-bolder">
                                         {{ ($clr_total['day2_total'] > 0) ? number_format($clr_total['day2_total']) : '-' }} | <span class="text-secondary"> 12,868,543</span>
-<<<<<<< HEAD
-                                        
-=======
-                                        <p class="mb-0 txt-last-upd">Last update: 10:00</p>
->>>>>>> wip
                                     </h2>
 
                                 </div>
@@ -71,12 +66,8 @@
                                 <div class="numbers">
                                     <p class="text-sm mb-0 text-uppercase font-weight-bold">Day 3</p>
                                     <h2 class="font-weight-bolder">
-<<<<<<< HEAD
                                         {{ ($clr_total['day3_total'] > 0) ? number_format($clr_total['day3_total']) : '-' }}  | <span class="text-secondary"> 20,687,940</span>
                                         <p class="mb-0" style="position: absolute;bottom: 4px;right: 20px;font-size: 14px;">Last update: 10:00</p>
-=======
-                                        {{ ($clr_total['day3_total'] > 0) ? number_format($clr_total['day3_total']) : '-' }}
->>>>>>> wip
                                     </h2>
 
                                 </div>
