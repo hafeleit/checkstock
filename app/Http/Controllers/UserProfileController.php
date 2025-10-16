2986415aa27b725505593d26558d2e549f1d71f1
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2025-10-16 14:09:46 +0700

    :lock: sec: prevent parameter tampering on profile update

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -14,32 +14,25 @@ class UserProfileController extends Controller
 
     public function update(Request $request)
     {
+        // Validate
         $attributes = $request->validate([
-            'username' => ['required','max:255', 'min:2'],
             'firstname' => ['max:100'],
             'lastname' => ['max:100'],
-            'email' => ['required', 'email', 'max:255',  Rule::unique('users')->ignore(auth()->user()->id),],
             'address' => ['max:100'],
             'city' => ['max:100'],
             'country' => ['max:100'],
             'postal' => ['max:100'],
             'about' => ['max:255'],
-            'supp_code' => ['max:255']
         ]);
 
-        auth()->user()->update([
-            'username' => $request->get('username'),
-            'firstname' => $request->get('firstname'),
-            'lastname' => $request->get('lastname'),
-            'email' => $request->get('email') ,
-            'address' => $request->get('address'),
-            'city' => $request->get('city'),
-            'country' => $request->get('country'),
-            'postal' => $request->get('postal'),
-            'about' => $request->get('about'),
-            'supp_code' => $request->get('supp_code')
-        ]);
+        // Authorize the user
+        if (!auth()->user()) {
+            abort(403, 'Unauthorized action.');
+        }
 
-        return back()->with('succes', 'Profile succesfully updated');
+        // Update
+        auth()->user()->update($attributes);
+
+        return back()->with('success', 'Profile successfully updated');
     }
 }
--- resources/views/pages/user-profile.blade.php
+++ resources/views/pages/user-profile.blade.php
@@ -35,7 +35,7 @@
                         <div class="card-header pb-0">
                             <div class="d-flex align-items-center">
                                 <p class="mb-0">Edit Profile</p>
-                                <button type="submit" class="btn btn-primary btn-sm ms-auto">Save</button>
+
                             </div>
                         </div>
                         <div class="card-body">
@@ -44,25 +44,29 @@
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Username</label>
-                                        <input class="form-control" type="text" name="username" value="{{ old('username', auth()->user()->username) }}" readonly>
+                                        <input class="form-control" type="text" name="username"
+                                            value="{{ old('username', auth()->user()->username) }}" readonly>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Email address</label>
-                                        <input class="form-control" type="email" name="email" value="{{ old('email', auth()->user()->email) }}" readonly>
+                                        <input class="form-control" type="email" name="email"
+                                            value="{{ old('email', auth()->user()->email) }}" readonly>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">First name</label>
-                                        <input class="form-control" type="text" name="firstname"  value="{{ old('firstname', auth()->user()->firstname) }}">
+                                        <input class="form-control" type="text" name="firstname"
+                                            value="{{ old('firstname', auth()->user()->firstname) }}">
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Last name</label>
-                                        <input class="form-control" type="text" name="lastname" value="{{ old('lastname', auth()->user()->lastname) }}">
+                                        <input class="form-control" type="text" name="lastname"
+                                            value="{{ old('lastname', auth()->user()->lastname) }}">
                                     </div>
                                 </div>
                             </div>
@@ -79,19 +83,22 @@
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">City</label>
-                                        <input class="form-control" type="text" name="city" value="{{ old('city', auth()->user()->city) }}">
+                                        <input class="form-control" type="text" name="city"
+                                            value="{{ old('city', auth()->user()->city) }}">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Country</label>
-                                        <input class="form-control" type="text" name="country" value="{{ old('country', auth()->user()->country) }}">
+                                        <input class="form-control" type="text" name="country"
+                                            value="{{ old('country', auth()->user()->country) }}">
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Postal code</label>
-                                        <input class="form-control" type="text" name="postal" value="{{ old('postal', auth()->user()->postal) }}">
+                                        <input class="form-control" type="text" name="postal"
+                                            value="{{ old('postal', auth()->user()->postal) }}">
                                     </div>
                                 </div>
                             </div>
@@ -101,16 +108,27 @@
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Country</label>
-                                        <select class="form-control" name="country" data-toggle="select" title="Simple select" data-live-search="true" data-live-search-placeholder="Search ...">
-                                            <option value="HVN" {{ (auth()->user()->about == 'HTH') ? 'selected' : '' }}>HTH</option>
-                                            <option value="HVN" {{ (auth()->user()->about == 'HVN') ? 'selected' : '' }}>HVN</option>
-                                            <option value="HSI" {{ (auth()->user()->about == 'HSI') ? 'selected' : '' }}>HSI</option>
-                                            <option value="HIN" {{ (auth()->user()->about == 'HIN') ? 'selected' : '' }}>HIN</option>
-                                            <option value="HSR" {{ (auth()->user()->about == 'HSR') ? 'selected' : '' }}>HSR</option>
-                                            <option value="HMA" {{ (auth()->user()->about == 'HMA') ? 'selected' : '' }}>HMA</option>
-                                            <option value="HRI" {{ (auth()->user()->about == 'HRI') ? 'selected' : '' }}>HRI</option>
-                                            <option value="HPI" {{ (auth()->user()->about == 'HPI') ? 'selected' : '' }}>HPI</option>
-                                            <option value="HTW" {{ (auth()->user()->about == 'HTW') ? 'selected' : '' }}>HTW</option>
+                                        <select class="form-control" name="about" data-toggle="select"
+                                            title="Simple select" data-live-search="true"
+                                            data-live-search-placeholder="Search ...">
+                                            <option value="HTH" {{ auth()->user()->about == 'HTH' ? 'selected' : '' }}>
+                                                HTH</option>
+                                            <option value="HVN" {{ auth()->user()->about == 'HVN' ? 'selected' : '' }}>
+                                                HVN</option>
+                                            <option value="HSI" {{ auth()->user()->about == 'HSI' ? 'selected' : '' }}>
+                                                HSI</option>
+                                            <option value="HIN" {{ auth()->user()->about == 'HIN' ? 'selected' : '' }}>
+                                                HIN</option>
+                                            <option value="HSR" {{ auth()->user()->about == 'HSR' ? 'selected' : '' }}>
+                                                HSR</option>
+                                            <option value="HMA" {{ auth()->user()->about == 'HMA' ? 'selected' : '' }}>
+                                                HMA</option>
+                                            <option value="HRI" {{ auth()->user()->about == 'HRI' ? 'selected' : '' }}>
+                                                HRI</option>
+                                            <option value="HPI" {{ auth()->user()->about == 'HPI' ? 'selected' : '' }}>
+                                                HPI</option>
+                                            <option value="HTW" {{ auth()->user()->about == 'HTW' ? 'selected' : '' }}>
+                                                HTW</option>
                                         </select>
                                     </div>
                                 </div>
@@ -120,17 +138,20 @@
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="example-text-input" class="form-control-label">Supplier code</label>
-                                        <input class="form-control" type="text" name="supp_code" value="{{ old('postal', auth()->user()->supp_code) }}" readonly>
+                                        <input class="form-control" type="text" name="supp_code"
+                                            value="{{ old('postal', auth()->user()->supp_code) }}" readonly>
                                     </div>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="d-flex align-items-center">
-                                    <a href="{{ route('change-password')}}" type="button" class="btn btn-dark btn-sm ms-auto">Change Password</a>
-                                </div>
+                            <div class="card-footer d-flex justify-content-end align-items-center px-0">
+                                <a href="{{ route('change-password') }}" type="button"
+                                    class="btn btn-dark btn-sm me-2 mb-0">
+                                    Change Password
+                                </a>
+                                <button type="submit" class="btn btn-primary btn-sm mb-0">Save</button>
                             </div>
                         </div>
-
+                    </form>
                 </div>
             </div>
         </div>
