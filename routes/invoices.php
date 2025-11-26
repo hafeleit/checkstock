c7f20c2f74de336fb9b0af9e494aa302b1544aee
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2025-11-26 14:59:41 +0700

    :bug: fix(auth): protect /delivery-trackings route

--- routes/invoices.php
+++ routes/invoices.php
@@ -10,6 +10,8 @@ Route::middleware(['auth', 'check.status'])
     ->prefix('delivery-trackings')
     ->name('delivery-trackings.')
     ->group(function () {
+        Route::resource('/', InvTrackingController::class)->except(['create', 'show']);
+
         Route::get('/details', [InvTrackingController::class, 'detail'])->name('details');
         Route::get('/export-overall', [InvTrackingController::class, 'exportOverall'])->name('export-overall');
         Route::get('/export-pending', [InvTrackingController::class, 'exportPending'])->name('export-pending');
@@ -24,4 +26,4 @@ Route::middleware(['auth', 'check.status'])
         Route::resource('/imports', ImportController::class)->only('index', 'store');
     });
 
-Route::resource('delivery-trackings', InvTrackingController::class)->except(['create', 'show']);
+
