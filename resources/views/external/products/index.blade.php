cf40a83edf8f25976788a4423ea84d3269949cbe
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2026-03-31 14:39:37 +0700

    :frame_with_picture: feat: add no-image

--- resources/views/external/products/index.blade.php
+++ resources/views/external/products/index.blade.php
@@ -53,7 +53,22 @@
                     <div class="flex flex-col md:flex-row gap-6">
                         <div class="w-full md:w-2/5 2xl:w-1/5">
                             <div class="aspect-square bg-gray-50 rounded-lg overflow-hidden border border-gray-200 mb-4">
-                                <img src="{{ asset('/storage/img/products/' . $item_code . '.jpg') }}" alt="product image" class="w-full h-full object-contain p-2">
+                                @php
+                                    $fileImgPath = storage_path('app/public/img/products/' . $item_code . '.jpg');
+                                @endphp
+                                @if (File::exists($fileImgPath) && $item_code)
+                                    <img src="{{ asset('/storage/img/products/' . $item_code . '.jpg') }}" alt="product image" class="w-full h-full object-contain p-2">
+                                @else
+                                    <div class="flex flex-col h-36 items-center justify-center w-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-image text-gray-500" viewBox="0 0 16 16">
+                                        <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
+                                        <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+                                    </svg>
+                                    <p class="text-gray-500 m-0">No Image</p>
+                                </div>
+                                @endif
+                                
+                                
                             </div>
                             <a href="/customer/products/product-info/{{ $item_code }}" target="_blank" class="block w-full text-center bg-blue-900 hover:bg-gray-700 text-white font-medium py-1 px-4 rounded-lg transition duration-200">
                                 Product Information
