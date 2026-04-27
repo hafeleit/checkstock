7c0f0c19c76370519fc64c2105112d6d3a4b547f
rattanaporn-chp <rattanaporn.chomphoo@hafele.co.th>
2026-04-27 11:35:53 +0700

    :art: feat: move alert into card

--- resources/views/pages/consumerlabel/productitems/index.blade.php
+++ resources/views/pages/consumerlabel/productitems/index.blade.php
@@ -11,13 +11,15 @@
         }
 
     </style>
-    <div id="alert">
-        @include('components.alert')
-    </div>
+
     <div class="container-fluid py-4">
         <div class="row">
             <div class="col-12 px-0">
                 <div class="card">
+                    <div class="px-3">
+                        @include('components.alert')
+                    </div>
+                    
                     <div class="card-header pb-0">
                         <div class="d-lg-flex">
                             <div>
